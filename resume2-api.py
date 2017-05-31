b16a3d6c1a6398e5514dfcff46c3a4156fd09cd3
ttiaga <ttiaga@gmail.com>
2017-05-31 08:38:24 +0000

    update path to files

--- resume2-api.py
+++ resume2-api.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python
 from bottle import run, route
 from bottle import static_file
 @route('/<filename>')
 def server_static(filename):
-    return static_file(filename, root='')
+    return static_file(filename, root='/home/ttiaga/api/Resume')
 run(host='10.0.0.4', port=8080, debug=True)
